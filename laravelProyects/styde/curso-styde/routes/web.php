4131349347778ae8cd0f614d6a71e307afdc9595
Jdeleons <jdeleons3@miumg.edu.gt>
2018-07-31 21:27:12 -0600

    Rutas con Laravel

--- laravelProyects/styde/curso-styde/routes/web.php
+++ laravelProyects/styde/curso-styde/routes/web.php
@@ -1,16 +1,32 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', function(){
+		return 'Hola mundo';
+});
+
+Route::get('/usuarios', function(){
+	return 'Usuarios';
+});
+
+Route::get('/usuarios/new', function(){
+	return 'crear nuevo usuario';
+});
+
+Route::get('/usuarios/{id}', function($id){
+	return "Mostrando detalle del usuario: {$id}";
+})->where('id','\d+');
+
+Route::get('/saludo/{name}/{nickname?}', function($name,$nickname=null){
+	if ($nickname) {
+
+		return "Bienvenido {$name}, tu apodo es {$nickname}";
+	}else {
+		return "Bienvenido {$name}, no tienes apodo:";
+	}
 });
